c191ba42caacfa83872f39cbef96f34cce643837
jawwadkareem <karimjawwad09@gmail.com>
2025-09-12 23:54:17 +0500

    Navbar UI updated

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -81,7 +81,6 @@ export default function Navbar() {
             className="flex items-center space-x-2"
           >
             <Menu className="w-6 h-6" />
-            <span className="hidden sm:inline font-semibold">Menu</span>
           </button>
 
           {/* Center - Logo */}
@@ -98,10 +97,10 @@ export default function Navbar() {
 
           {/* Right - Contact */}
           <div className="flex items-center">
-            <a href="tel:+61493717475" className="flex flex-col items-end">
-              <span className="font-bold">0493 717 475</span>
-              <span className="text-xs">Monday to Friday</span>
-              <span className="text-xs">9am to 5pm</span>
+            <a href="tel:+61493717475" className="flex items-center">
+              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"></path>
+              </svg>
             </a>
           </div>
         </div>
