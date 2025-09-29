566d751bf0af4b4558239ae6e429872b415aa7c3
HUZAIFA <karimjawwad09@gmail.com>
2025-09-29 11:19:41 +0500

    Video added

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -28,7 +28,7 @@ const Home = () => {
   return (
     <div className="bg-white">
       <Banner
-        video="https://collectorcardepot.com/wp-content/uploads/2025/02/3066415-uhd_4096_2160_24fps.mp4"
+        video="/3066415-uhd_4096_2160_24fps.mp4"
         title="Welcome To Collector Car Depot"
         subtitle="Collector & Performance Car Brokers"
       />
--- src/app/Sold/page.tsx
+++ src/app/Sold/page.tsx
@@ -7,7 +7,7 @@ const page = () => {
   return (
     <div className='bg-white'>
     <Banner
-        video="https://collectorcardepot.com/wp-content/uploads/2025/02/3066415-uhd_4096_2160_24fps.mp4"
+        video="/3066415-uhd_4096_2160_24fps.mp4"
         title="Welcome To Collector Car Depot"
         subtitle="Collector & Performance Car Brokers"
       />
